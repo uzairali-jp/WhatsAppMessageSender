79b4718938158804efdf36b691275048b208b1fe
Uzair Ali <ua4220184@gmail.com>
2026-01-03 18:38:55 +0500

    UI updated.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function sendWhatsApp() {
+function sendBtn() {
     let phone = document.getElementById("phone").value;
     let address = document.getElementById("address").value;
 
@@ -9,8 +9,61 @@ function sendWhatsApp() {
     }
 
     let message = address;
+            // let url = `https://web.whatsapp.com/send?phone=${fullNumber}&text=${encodeURIComponent(message)}`;
 
-    let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+    let url = "https://web.whatsapp.com/send?phone" + phone + "?text=" + encodeURIComponent(message);
 
     window.open(url, "_blank");
 }
+
+
+
+
+
+
+ document.addEventListener("DOMContentLoaded", function() {
+      const input = document.querySelector("#phone");
+
+      window.intlTelInput(input, {
+        initialCountry: "pk",    
+        separateDialCode: false, 
+        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js" // formatting/validation
+      });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sendWhatsApp() {
+//     let phone = document.getElementById("phone").value;
+//     let address = document.getElementById("address").value;
+
+
+//     if (phone === "" || address === "") {
+//         alert("Please enter phone number and address");
+//         return;
+//     }
+
+//     let message = address;
+
+//     let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+
+//     window.open(url, "_blank");
+// }
